42a341935d7466658f9990355077127187fc36a5
Max Einhorn <maxeinhorn@gmail.com>
2021-03-19 22:46:03 -0700

    Test working

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Put the input file in right spot
 Run "node entryPoint"
+
+To test, navigate to the directory in which you have cloned this project and run: npm test
--- test/test.js
+++ test/test.js
@@ -1,17 +1,29 @@
-var assert = require('assert');
+const assert = require('assert');
+const calculator = require('../calculator.js');
 
-describe('Array', function() {
+const target = 720;
+const matrix = [
+  [2],
+  [4,3],
+  [3,2,6],
+  [2,9,5,2],
+  [10,5,2,15,5],
+];
+
+// console.log('HELLO: ', calculator.calc(target, matrix));
+
+describe('Problem', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
+      assert.equal(calculator.calc(target, matrix), 'LRLL');
+    });
+  });
+
+  describe('Output maps to working result', function() {
+    it('should return true when the products of the path equal the target', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
+
 });
 
-describe('Path results in accurate ', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
