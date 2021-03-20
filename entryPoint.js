6dcfebeea457778f0a6ed8bdbd81ac1970ef0a51
Max Einhorn <maxeinhorn@gmail.com>
2021-03-19 21:09:00 -0700

    successfully reading input .txt

---
+++ entryPoint.js
@@ -0,0 +1,4 @@
+const inputs = require('./input.js');
+
+console.log(inputs.target);
+console.log(inputs.matrix);
--- input.js
+++ input.js
@@ -6,35 +6,76 @@ fs = require('fs');
 
 var file = '/Users/maxeinhorn/coding/AoPS/pyramid_sample_input.txt';
 
-fs.readFile(file, 'utf8', function(err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  // console.log(data);
+// fs.readFile(file, 'utf8', function(err, data) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   var lines = data.split('\r\n');
+//   var targetString = lines[0].split(' ')[1];
+//   var targetNumber = parseInt(targetString);
+
+//   var matrixArrays = [];
+//   var finalMatrix = [];
+//   for (var i = 1; i < lines.length; i++) {
+//     matrixArrays.push(lines[i]);
+//   }
+//   matrixArrays.pop();
+//   for (var j = 0; j < matrixArrays.length; j++) {
+//     var row = [];
+//     var arrayify = matrixArrays[j].split(',');
+//     finalMatrix.push(arrayify);
+//   }
+//   for (var k = 0; k < finalMatrix.length; k++) {
+//     for (var l = 0; l < finalMatrix[k].length; l++) {
+//       finalMatrix[k][l] = parseInt(finalMatrix[k][l]);
+//     }
+//   }
+//   // console.log('finalMatrix: ', finalMatrix);
+//   // console.log('targetNumber: ', targetNumber);
+// })
+
+var getData = function(filePath) {
+  var data = fs.readFileSync(filePath, 'utf8');
+  return data;
+}
+
+var getTarget = function(data) {
   var lines = data.split('\r\n');
   var targetString = lines[0].split(' ')[1];
-  var targetNumber = parseInt(targetString);
+  var targetNum = parseInt(targetString);
+  // console.log('targetNum: ', targetNum)
+  return targetNum;
+}
 
+var getMatrix = function(data) {
+  var lines = data.split('\r\n');
   var matrixArrays = [];
   var finalMatrix = [];
-  // console.log('lines: ', lines);
   for (var i = 1; i < lines.length; i++) {
     matrixArrays.push(lines[i]);
   }
   matrixArrays.pop();
   for (var j = 0; j < matrixArrays.length; j++) {
     var row = [];
-    console.log('matrixArrays[j]: ', matrixArrays[j]);
     var arrayify = matrixArrays[j].split(',');
     finalMatrix.push(arrayify);
   }
   for (var k = 0; k < finalMatrix.length; k++) {
     for (var l = 0; l < finalMatrix[k].length; l++) {
-      // console.log(finalMatrix[k][l]);
       finalMatrix[k][l] = parseInt(finalMatrix[k][l]);
     }
   }
-  console.log('matrixArrays: ', matrixArrays);
-  console.log('finalMatrix: ', finalMatrix);
+  // console.log('finalMatrix: ', finalMatrix);
   // console.log('targetNumber: ', targetNumber);
-})
+  return finalMatrix;
+}
+
+var data = getData(file);
+var target = getTarget(data);
+// console.log('TARGET: ', target);
+var matrix = getMatrix(data);
+// console.log('MATRIX: ', matrix);
+
+// var matrix = getMatrix();
+
+module.exports = {target: target, matrix: matrix}
